df2d081b1e53e91bfa842d1ff6944288e9b3a838
ellywonght <ellywonght@gmail.com>
2025-05-15 19:12:50 +0000

    Updates

---
+++ precache-manifest.eb8964698979119e534b1e6f493a3d1a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d8b3e14ed6384b431f3ce4352a6b18c",
+    "url": "/alzheimers-detection-app/index.html"
+  },
+  {
+    "revision": "196c4e9b4ca11c1ceb08",
+    "url": "/alzheimers-detection-app/static/css/main.8cc4582c.chunk.css"
+  },
+  {
+    "revision": "df530c6d8480a540b2b9",
+    "url": "/alzheimers-detection-app/static/js/2.2cb2b905.chunk.js"
+  },
+  {
+    "revision": "5f94b84929db496b4663",
+    "url": "/alzheimers-detection-app/static/js/3.1352c071.chunk.js"
+  },
+  {
+    "revision": "196c4e9b4ca11c1ceb08",
+    "url": "/alzheimers-detection-app/static/js/main.1c2cefa1.chunk.js"
+  },
+  {
+    "revision": "31b3db6930132e93c551",
+    "url": "/alzheimers-detection-app/static/js/runtime~main.4509bbfd.js"
+  }
+]);
